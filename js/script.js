ec5e43d4254853b39320ff310cbe9aa199d273c8
Tim-Thiel <braunsilas7@gmail.com>
2025-11-14 11:32:42 +0100

    Implement image download as ZIP functionality
    Adds a function to download selected images as a ZIP file.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
+async function downloadSelected() {
+    const checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+    if (checkboxes.length === 0) {
+        alert("Bitte wählen Sie mindestens ein Bild aus.");
+        return;
+    }
+
+    const zip = new JSZip();
+    const folder = zip.folder("ausgewählte_bilder");
+
+    for (let box of checkboxes) {
+        const url = box.value;
+        const filename = url.split("/").pop();
+
+        const response = await fetch(url);
+        const blob = await response.blob();
+        
+        folder.file(filename, blob);
+    }
+
+    const content = await zip.generateAsync({ type: "blob" });
+
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(content);
+    link.download = "bilder.zip";
+    link.click();
+}
 
