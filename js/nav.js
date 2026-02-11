70bf424bfa12e6787b82f8b3d0e05350539ea588
Tim-Thiel <braunsilas7@gmail.com>
2026-02-11 19:11:18 +0100

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -58,7 +58,7 @@ const footerHTML = `
         <div class="footer-sec">
             <h3>Kontakt</h3>
             <p>📧 <a href="mailto:tim_thiel@r-rangers.de" class="footer-link-white">tim_thiel@r-rangers.de</a></p>
-            <p><a href="https://www.r-rangers.de" target="_blank"></p>
+            <p><a href="https://www.r-rangers.de" target="_blank">www.r-rangers.de</a></p>
         </div>
 
         <div class="footer-sec">
